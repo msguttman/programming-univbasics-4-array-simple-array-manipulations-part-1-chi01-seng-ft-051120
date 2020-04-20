6a0407896dbeee48de9a1ae37c9c376bffa3c246
Matt Guttman <msguttman@gmail.com>
2020-04-20 21:20:25 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,26 @@
+
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array, numb)
+  array.pop(numb)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array, numb)
+  array.shift(numb)
+end
+
+
